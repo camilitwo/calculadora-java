7904a3c9f180d73351e0c29645c65355bef0ba5a
Camilo Gonzalez V <camilo.igv@gmail.com>
2025-05-10 22:19:42 -0400

    Add files via upload
    se agrega proyecto inicial

---
+++ src/test/java/com/clases/fran/calculadorajava/CalculadoraJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clases.fran.calculadorajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
